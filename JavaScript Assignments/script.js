8ed205b592cdea75824fc08976a957ebc63a3046
Muhammad Arslan <116803961+Arslan4106@users.noreply.github.com>
2026-07-14 10:21:19 +0500

    Add files via upload

---
+++ JavaScript Assignments/script.js
@@ -0,0 +1,6 @@
+let country = "Pakistan";
+let population = "228";
+let continent = "Asia";
+console.log("My Country Name is ", country);
+console.log(country, "Has the Popluation of", population, " Million People");
+console.log("The continent of", country, "is", continent);
