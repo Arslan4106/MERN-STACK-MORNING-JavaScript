107eb85170d509b5fe126ef13472182e9b2341cb
Muhammad Arslan <116803961+Arslan4106@users.noreply.github.com>
2026-08-11 09:59:10 +0500

    Add files via upload

--- DOM/Guess-My-Number/script.js
+++ DOM/Guess-My-Number/script.js
@@ -7,24 +7,48 @@ document.querySelector(".check").addEventListener("click", function () {
   let guess = document.querySelector(".numbers").value;
   console.log(guess);
   if (!guess) {
-    document.querySelector(".message").textContent = "No Number 😡";
-    document.querySelector("body").style.backgroundColor = "darkred";
-    score--; // score = score - 1 => score = 20 - 1 => score = 19;
-    document.querySelector(".score").textContent = score;
+    if (score > 1) {
+      document.querySelector(".message").textContent = "No Number 😡";
+      document.querySelector("body").style.backgroundColor = "darkred";
+      score--; // score = score - 1 => score = 20 - 1 => score = 19;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent =
+        "You Have Lost the Game 😭";
+      document.querySelector("body").style.backgroundColor = "darkred";
+      document.querySelector(".score").textContent = 0;
+    }
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "Too Large 📈";
-    score--; // score = score - 1 => score = 20 - 1 => score = 19;
-    document.querySelector(".score").textContent = score;
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too Large 📈";
+      score--; // score = score - 1 => score = 20 - 1 => score = 19;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent =
+        "You Have Lost the Game 😭";
+      document.querySelector("body").style.backgroundColor = "darkred";
+      document.querySelector(".score").textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "Too Low 📉";
-    score--; // score = score - 1 => score = 20 - 1 => score = 19;
-    document.querySelector(".score").textContent = score;
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too Low 📉";
+      score--; // score = score - 1 => score = 20 - 1 => score = 19;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent =
+        "You Have Lost the Game 😭";
+      document.querySelector("body").style.backgroundColor = "darkred";
+      document.querySelector(".score").textContent = 0;
+    }
   } else if (guess == secretNumber) {
     document.querySelector(".message").textContent = "👉 Correct Number";
     document.querySelector("body").style.backgroundColor = "green";
+    document.querySelector(".secretNumber").textContent = secretNumber;
   }
-
   // console.log("You have Clicked on the Button");
   // console.log("YOu have Clicked");
 });
 // console.log(check);
+document.querySelector(".again").addEventListener("click", function () {
+  document.querySelector("body").style.backgroundColor = "rgb(36, 36, 36)";
+});
