b12f79286f71e6dfab627ede9d7bdff49c0af02e
Muhammad Arslan <116803961+Arslan4106@users.noreply.github.com>
2026-08-21 09:52:59 +0500

    Add files via upload

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -1,4 +1,6 @@
 // Storing all the elements into variables
+// let player0El = document.querySelector(".player--0");
+// let player1El = document.querySelector(".player--1");
 const scoreEl0 = document.getElementById("score--0");
 console.log(scoreEl0);
 const scoreEl1 = document.getElementById("score--1");
@@ -15,9 +17,19 @@ const btnRoll = document.querySelector(".rolldice");
 console.log(btnRoll);
 const btnHold = document.querySelector(".hold");
 console.log(btnHold);
-let currentScore = 0;
+// const player0El = document.querySelector(".player--0");
+// const player1El = document.querySelector(".player--1");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 scoreEl0.textContent = 0;
 scoreEl1.textContent = 0;
+// const scores = [0, 0];
+let currentScore = 0;
+// let activePlayer = 0;
+// let activePlayer = 0;
+// const scores = [0, 0];
+let activePlayer = 0;
+const scores = [0, 0];
 btnRoll.addEventListener("click", function () {
   // 1. Generating a Random number b/w 1 & 6
   let dice = Math.trunc(Math.random() * 6) + 1;
@@ -28,8 +40,33 @@ btnRoll.addEventListener("click", function () {
   //dice-5.png;
   if (dice !== 1) {
     currentScore = currentScore + dice;
-    currentEl0.textContent = currentScore;
+    // document.getElementById(`current--${activePlayer}`).textContent =
+    //   currentScore;
+    // document.getElementById(`current--${activePlayer}`).textContent =
+    //   currentScore;
+    // currentEl0.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Player Switch
+    // if (activePlayer === 0) {
+    //   activePlayer = 1;
+    // } else {
+    //   activePlayer = 0;
+    // }
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
+    // currentScore = 0;
+    // document.getElementById(`current--${activePlayer}`).textContent = 0;
+    // activePlayer = activePlayer === 0 ? 1 : 0;
+    // player0El.classList.toggle("player--active");
+    // player1El.classList.toggle("player--active");
+    // document.getElementById(`current--${activePlayer}`).textContent = 0;
+    // activePlayer = activePlayer === 0 ? 1 : 0;
+    // player0El.classList.toggle("player--active");
+    // player1El.classList.toggle("player--active");
   }
 });
